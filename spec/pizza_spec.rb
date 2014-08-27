d3d5d7a199b0583de79d741c1eba7222476f2468
Marco Andre Lostaunau <lostaunaum@gmail.com>
2014-08-27 17:03:02 -0500

    THis pizza spec rb will contain all the tests for a successful code

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -10,4 +10,27 @@ describe Topping do
   it "exists" do
     expect(Topping).to be_a(Class)
   end
+  describe Pizza do
+  describe '.initialize' do
+    it 'records all of the toppings' do
+      toppings = [
+        Topping.new('mushrooms', vegetarian: true),
+        Topping.new('pepperoni')
+      ]
+      pizza = Pizza.new(toppings)
+
+      expect(pizza.toppings).to eq(toppings)
+      end 
+    end 
+  end
+  describe '.initialize' do
+    it 'sets whether or not the topping is vegetarian' do
+      topping = Topping.new 'bell peppers', vegetarian: true
+      expect(topping.vegetarian).to eq(true)
+    end
+    it 'sets the name of the topping' do
+      topping = Topping.new('olives')
+      expect(topping.name).to eq('olives')
+    end
+  end
 end
